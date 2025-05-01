1439264973076fa73488b07a232ef41204baac63
felipe.silva <felipe.silva@pwi.com.br>
2025-05-01 13:50:57 -0300

    feat: Criando componente de título

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TitleComponent } from './components/title/title.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TitleComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    TitleComponent
   ]
 })
 export class SharedModule { }
