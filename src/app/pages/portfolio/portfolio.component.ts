9f0d81fd0076551c9bd39b884927c71b842bb185
felipe.silva <felipe.silva@pwi.com.br>
2025-05-02 13:47:54 -0300

    feat: Criando página de portfólio

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProjectListComponent } from "../../modules/project/components/project-list/project-list.component";
+import { SharedModule } from "../../shared/shared.module";
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [ProjectListComponent],
+  imports: [ProjectListComponent, SharedModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
