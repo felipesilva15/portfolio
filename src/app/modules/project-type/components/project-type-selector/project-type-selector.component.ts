527dc9aa7a20a0f6189e4257117b134fdcafa7d7
felipe.silva <felipe.silva@pwi.com.br>
2025-05-13 13:45:52 -0300

    feat: Implementando seletor de tipo de projeto responsivo com signals

--- src/app/modules/project-type/components/project-type-selector/project-type-selector.component.ts
+++ src/app/modules/project-type/components/project-type-selector/project-type-selector.component.ts
@@ -1,21 +1,29 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, effect, EventEmitter, Output, signal, WritableSignal } from '@angular/core';
 import { ProjectType } from '../../../../models/project-type.model';
 import { ProjectTypeService } from '../../services/project-type.service';
 import { ButtonModule } from 'primeng/button';
+import { SelectModule } from 'primeng/select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-project-type-selector',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, SelectModule, FormsModule],
   templateUrl: './project-type-selector.component.html',
   styleUrl: './project-type-selector.component.scss'
 })
 export class ProjectTypeSelectorComponent {
   projectTypes!: ProjectType[];
-  selectedProjectType?: ProjectType;
+  selectedProjectType: WritableSignal<ProjectType | undefined> = signal<ProjectType | undefined>(undefined);
   @Output() selectProjectTypeEvent = new EventEmitter<ProjectType>();
 
-  constructor(private projectTypeService: ProjectTypeService) { }
+  constructor(private projectTypeService: ProjectTypeService) {
+    effect(() => {
+      if (this.selectedProjectType()) {
+        this.onProjectTypeSelected();
+      }
+    })
+  }
 
   ngOnInit(): void {
     this.loadData();
@@ -30,13 +38,12 @@ export class ProjectTypeSelectorComponent {
           name: 'Todos'
         });
 
-        this.onProjectTypeSelected(this.projectTypes[0]);
+        this.selectedProjectType.set(this.projectTypes[0]);
       }
     });
   }
 
-  onProjectTypeSelected(projectType: ProjectType): void {
-    this.selectedProjectType = projectType;
-    this.selectProjectTypeEvent.emit(this.selectedProjectType);
+  onProjectTypeSelected(): void {
+    this.selectProjectTypeEvent.emit(this.selectedProjectType());
   }
 }
