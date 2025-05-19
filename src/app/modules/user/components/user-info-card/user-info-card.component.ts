bdc5db601e46cb4bd768bfeb57c0c77091845c1b
felipe.silva <felipe.silva@pwi.com.br>
2025-05-19 18:57:24 -0300

    feat: Deixando o card de informações responsivo

--- src/app/modules/user/components/user-info-card/user-info-card.component.ts
+++ src/app/modules/user/components/user-info-card/user-info-card.component.ts
@@ -1,25 +1,41 @@
-import { Component, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, computed, OnInit, Signal, signal, WritableSignal } from '@angular/core';
 import { User } from '../../../../models/user.model';
 import { UserService } from '../../services/user.service';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
 import { TagModule } from 'primeng/tag';
 import { PipesModule } from '../../../../shared/pipes/pipes.module';
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { SharedModule } from '../../../../shared/shared.module';
 
 @Component({
   selector: 'app-user-info-card',
   standalone: true,
-  imports: [TagModule, ButtonModule, DividerModule, PipesModule, DatePipe, SharedModule],
+  imports: [
+    TagModule, 
+    ButtonModule, 
+    DividerModule, 
+    PipesModule, 
+    DatePipe, 
+    SharedModule,
+    CommonModule
+  ],
   templateUrl: './user-info-card.component.html',
   styleUrl: './user-info-card.component.scss'
 })
 export class UserInfoCardComponent implements OnInit {
   user!: User;
-  moreInfoActive: WritableSignal<boolean> = signal<boolean>(false);
+  expandMoreInfo: WritableSignal<boolean> = signal<boolean>(false);
+  
+  windowWidth: WritableSignal<number> = signal<number>(window.innerWidth)
+  isLargeScreen: Signal<boolean> = computed(() => this.windowWidth() >= 1200);
+  showMoreInfo: Signal<boolean> = computed(() => (!this.isLargeScreen() && this.expandMoreInfo()) || this.isLargeScreen());
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {
+    window.addEventListener('resize', () => {
+      this.windowWidth.set(window.innerWidth);
+    });
+  }
 
   ngOnInit(): void {
     this.loadData();
@@ -33,7 +49,7 @@ export class UserInfoCardComponent implements OnInit {
     });
   }
 
-  toogleMoreInfo(): void {
-    this.moreInfoActive.update(value => !value);
+  toogleExpandMoreInfo(): void {
+    this.expandMoreInfo.update(value => !value);
   }
 }
