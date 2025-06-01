df6f9a866210b8dbc5c87b81b0f8fc70810fc64c
Felipe Silva <felipe.allware@gmail.com>
2025-06-01 13:36:16 -0300

    feat: Adicionando carregamento e skeleton para o user card e about page

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal, WritableSignal } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { UserService } from '../../modules/user/services/user.service';
 import { User } from '../../models/user.model';
+import { finalize } from 'rxjs';
+import { SkeletonModule } from 'primeng/skeleton';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [SharedModule],
+  imports: [
+    SharedModule,
+    SkeletonModule
+  ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent implements OnInit {
   user!: User;
+  isLoading: WritableSignal<boolean> = signal<boolean>(true);
 
   constructor(private userService: UserService) { }
 
@@ -20,7 +26,13 @@ export class AboutMeComponent implements OnInit {
   }
 
   loadData(): void {
-    this.userService.getById(1).subscribe({
+    this.isLoading.set(true);
+
+    this.userService.getById(1)
+    .pipe(
+      finalize(() => this.isLoading.set(false))
+    )
+    .subscribe({
       next: (res: User) => {
         this.user = res;
       }
