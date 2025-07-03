08c99073d4d239c141ad04085f9c805b710927ab
felipe.silva <felipe.silva@pwi.com.br>
2025-07-03 19:23:13 -0300

    test: finalizando testes do DynamicDialogService e ContactComponent

--- src/app/pages/contact/contact.component.spec.ts
+++ src/app/pages/contact/contact.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactComponent } from './contact.component';
+import { By } from '@angular/platform-browser';
+import { TitleComponent } from '../../shared/components/title/title.component';
+import { ContactService } from '../../modules/contact/services/contact.service';
+import { DynamicDialogService } from '../../shared/services/dynamic-dialog.service';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
+  let mockContactService: jasmine.SpyObj<ContactService>;
+  let mockDynamicDialogService: jasmine.SpyObj<DynamicDialogService>;
 
   beforeEach(async () => {
+    mockContactService = jasmine.createSpyObj('ContactService', ['create']);
+    mockDynamicDialogService = jasmine.createSpyObj('DynamicDialogService', ['message']);
+
     await TestBed.configureTestingModule({
-      imports: [ContactComponent]
+      imports: [ContactComponent],
+      providers: [
+        { provide: ContactService, useValue: mockContactService },
+        { provide: DynamicDialogService, useValue: mockDynamicDialogService }
+      ]
     })
     .compileComponents();
 
@@ -17,7 +30,20 @@ describe('ContactComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+  
+  it('should render the TitleComponent', () => {
+    const titleComponentElement = fixture.debugElement.query(By.css('app-title'));
+    expect(titleComponentElement).toBeTruthy();
+
+    const titelComponent = titleComponentElement.componentInstance as TitleComponent;
+    expect(titelComponent.text).toBe('Contato');
+  });
+
+  it('should render the ContactFormComponent', () => {
+    const contactFormComponentElement = fixture.debugElement.query(By.css('app-contact-form'));
+    expect(contactFormComponentElement).toBeTruthy();
   });
 });
--- src/app/shared/services/dynamic-dialog.service.spec.ts
+++ src/app/shared/services/dynamic-dialog.service.spec.ts
@@ -1,16 +1,116 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DynamicDialogService } from './dynamic-dialog.service';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { MessageDialogComponent } from '../components/message-dialog/message-dialog.component';
+import { DynamicDialogConfig } from '../../models/dynamic-dialog-config.data';
+import { DialogSize } from '../enums/dialog-size';
+import { Subject } from 'rxjs';
+
+class DummyComponent {}
 
 describe('DynamicDialogService', () => {
   let service: DynamicDialogService;
+  let mockPrimengDialogService: jasmine.SpyObj<DialogService>;
+  let mockDialogRef: jasmine.SpyObj<DynamicDialogRef>;
+  let onCloseSubject: Subject<any>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockPrimengDialogService = jasmine.createSpyObj('DialogService', ['open']);
+    onCloseSubject = new Subject<any>();
+    mockDialogRef = jasmine.createSpyObj('DynamicDialogRef', ['onClose']);
+    mockDialogRef.onClose = onCloseSubject.asObservable();
+
+    mockPrimengDialogService.open.and.returnValue(mockDialogRef);
+
+    TestBed.configureTestingModule({
+      providers: [
+        DynamicDialogService,
+        { provide: DialogService, useValue: mockPrimengDialogService }
+      ]
+    });
+
     service = TestBed.inject(DynamicDialogService);
   });
 
-  it('should be created', () => {
+  it('shoud be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call the "open" method of the DialogService when opening a message dialog', () => {
+    mockPrimengDialogService.open.and.returnValue(mockDialogRef);
+
+    const message: string = 'Ocorreu um problema!';
+    const title: string = 'Atenção';
+    
+    service.message(message, title);
+
+    expect(mockPrimengDialogService.open).toHaveBeenCalledWith(
+      MessageDialogComponent,
+      jasmine.objectContaining({
+        data: { 
+          message: message
+        },
+        header: title
+      })
+    );
+  });
+
+  it('must call the open method of the PrimeNG DialogService with the correct parameters', () => {
+    const config: DynamicDialogConfig = {
+      title: 'Dummy',
+      data: { 
+        id: 1,
+        name: 'Exemplo'
+      },
+      closeable: true,
+      styleClass: 'custom-modal-class',
+      size: DialogSize.Large
+    };
+
+    service.open(DummyComponent, config);
+
+    expect(mockPrimengDialogService.open).toHaveBeenCalledWith(
+      DummyComponent,
+      jasmine.objectContaining({
+        header: config.title,
+        modal: true,
+        data: config.data,
+        closable: config.closeable,
+        styleClass: config.styleClass
+      })
+    );
+  });
+
+  it('must resolve the Promise with the value emitted by onClose', async () => {
+    const config: DynamicDialogConfig = {
+      title: 'Dummy',
+      size: DialogSize.Medium
+    };
+    const expectedResult = {
+      status: 'success',
+      message: 'Dados do modal'
+    };
+
+    const dialogPromise = service.open(DummyComponent, config);
+
+    onCloseSubject.next(expectedResult);
+    onCloseSubject.complete();
+
+    await expectAsync(dialogPromise).toBeResolvedTo(expectedResult);
+  });
+
+  it('should reject the Promise with the error thrown by onClose', async () => {
+    const config: DynamicDialogConfig = {
+      title: 'Dummy',
+      size: DialogSize.Small
+    };
+    const expectedError = new Error('Erro ao fechar o modal');
+
+    const dialogPromise = service.open(DummyComponent, config);
+
+    onCloseSubject.error(expectedError);
+
+    await expectAsync(dialogPromise).toBeRejectedWith(expectedError);
   });
 });
